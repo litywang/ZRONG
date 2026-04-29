7fbc19492866deb0c4bb3d46a7f1f26ccef49c4d
GitHub Action <action@github.com>
2026-04-29 00:04:59 +0000

    Update proxies 2026-04-29 00:04 [v28.39]

--- cn_cidr_data.py
+++ cn_cidr_data.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # CN CIDR blocks from APNIC (auto-generated)
 # Source: https://github.com/gaoyifan/china-operator-ip
-# Total: 4156 CIDR blocks | Generated: 2026-04-29 07:53:28.419487
+# Total: 4156 CIDR blocks | Generated: 2026-04-29 00:00:09.368767
 
 import ipaddress
 
